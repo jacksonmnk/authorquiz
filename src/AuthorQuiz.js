f787f4eccb98d0a73dd841996b5309c7ce0ccab1
Jackson Mwaniki <jackson.mwaniki@gmail.com>
2019-10-09 22:27:38 +0300

    Bootstrapping the Author Quiz

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+
+function AuthorQuiz() {
+  return (
+    <div>AuthorQuiz</div>
+  );
+}
+
+export default AuthorQuiz;
