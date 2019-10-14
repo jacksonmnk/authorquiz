67b9c3f876e0d92b6588ee660c511975115b2664
Jackson Mwaniki <jackson.mwaniki@gmail.com>
2019-10-14 17:10:55 +0300

    Create new components.

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import './bootstrap.min.css';
 
+function Hero() {
+    return (<div className="row">
+        <div className="jumbotron col-10 offset-1">
+            <h1>Author Quiz</h1>
+            <p>Select the book written by the author shown</p>
+        </div>
+    </div>);
+}
 
-function AuthorQuiz() {
-  return (
-    <div>AuthorQuiz</div>
-  );
+function Turn(){
+
+}
+
+function Continue(){
+
+}
+
+class AuthorQuiz extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <Hero/>
+                <Turn/>
+                <Continue/>
+            </div>
+        );
+    }
 }
 
 export default AuthorQuiz;
--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AuthorQuiz from './App';
+import AuthorQuiz from './AuthorQuiz';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
