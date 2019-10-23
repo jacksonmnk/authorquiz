1e7999104d6ad7d5d076020e78a923933607e5e6
Jackson Mwaniki <jackson.mwaniki@gmail.com>
2019-10-23 18:44:57 +0300

    Randomly select data and render for display.

--- src/index.js
+++ src/index.js
@@ -3,21 +3,64 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
 const authors = [
     {
+        id: 1,
         name: 'Mark Twain',
-        imageUrl: 'images/authors/marktwain.jpg',
-        imageSource: 'Wikimedia Commons',
-        books: ['The adventures of Huckleberry Finn']
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/25/Mark_Twain%2C_Brady-Handy_photo_portrait%2C_Feb_7%2C_1871%2C_cropped.jpg',
+        books: ['The Adventures of Huckleberry Finn']
+    },
+    {
+        id: 2,
+        name: 'Joseph Conrad',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/07/Joseph_Conrad.PNG',
+        books: ['Heart of Darkness']
+    },
+    {
+        id: 3,
+        name: 'J.K. Rowling',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5d/J._K._Rowling_2010.jpg',
+        books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+        id: 4,
+        name: 'Stephen King',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/e3/Stephen_King%2C_Comicon.jpg',
+        books: ['The Shining']
+    },
+    {
+        id: 5,
+        name: 'Charles Dickens',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Dickens_Gurney_head.jpg',
+        books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+    {
+        id: 6,
+        name: 'William Shakespeare',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Dickens_Gurney_head.jpg',
+        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
     }
 ];
 
-const state = {
-    turnData: {
-        authors: authors[0],
-        books: authors[0].books
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        authors: authors.find((authors) =>
+            authors.books.some((title) =>
+                title === answer))
     }
+}
+
+const state = {
+    turnData: getTurnData(authors)
 }
 
 ReactDOM.render(<AuthorQuiz {...state}/>, document.getElementById('root'));
